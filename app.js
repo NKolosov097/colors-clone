bb599cec3f0b3f121cb2df8dc3bcdf2dc01a703f
NKolosov097 <N.Kolosov2003@mail.ru>
2022-10-20 20:14:54 +0300

    init: start colors-clone-app

---
+++ app.js
@@ -0,0 +1,92 @@
+const cols = document.querySelectorAll('.col')
+
+document.addEventListener('keydown', event => {
+    event.preventDefault()
+    if (event.code.toLowerCase() === 'space') {
+        setRandomColors()
+    }
+})
+
+document.addEventListener('click', event => {
+    const type = event.target.dataset.type
+
+    if (type === 'lock') {
+        const node = event.target.tagName.toLowerCase() === 'i'
+            ? event.target
+            : event.target.children[0]
+
+        node.classList.toggle('fa-lock-open')
+        node.classList.toggle('fa-lock')
+    } else if (type === 'copy') {
+        copyToClickboard(event.target.textContent)
+    }
+})
+
+const generateRandomColor = () => {
+    const hexCodes = '0123456789ABCDEF'
+    let color = ''
+
+    for (let i = 0; i < 6; i++) {
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+
+    return '#' + color 
+}
+
+const copyToClickboard = (text) => {
+    return navigator.clipboard.writeText(text)
+}
+
+const setRandomColors = (isInitial) => {
+    const colors = isInitial ? getColorsFromHash() :  []
+
+    cols.forEach((col, index) => {
+        const islocked = col.querySelector('i').classList.contains('fa-lock')
+        const text = col.querySelector('h2')
+        const button = col.querySelector('button')
+
+        if (islocked) {
+            colors.push(text.textContent)
+            return
+        }
+
+        // const color = generateRandomColor()
+        const color = isInitial 
+        ? colors[index]
+            ? colors[index] 
+            : chroma.random()
+        : chroma.random()
+
+        if (!isInitial) {
+            colors.push(color)
+        }
+
+        text.textContent = color
+        col.style.background = color
+
+        setTextColor(text, color)
+        setTextColor(button, color)
+    })
+
+    updateColorsHash(colors)
+}
+
+const setTextColor = (text, color) => {
+    const luminance=  chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
+
+const updateColorsHash = (colors = []) => {
+    document.location.hash = colors.map(color => {
+        return color.toString().substring(1)
+    }).join('-')
+}
+
+const getColorsFromHash = () => {
+    if (document.location.hash.length > 1) {
+        return document.location.hash.substring(1).split('-').map(color => '#' + color)
+    }   
+    return []
+}
+
+setRandomColors(true)
